a3bc47122dae245f77ecde1b95d6b7dae7827479
shuh <nonya@biz.com>
2019-08-06 13:24:32 -0400

    started layout of mysql and database - to be revised

---
+++ client/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS guestrev_db;
+CREATE DATABASE guestrev_db;
+USE guestrev_db;
+
+CREATE TABLE property_list(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  companyName VARCHAR(225) NOT NULL,
+  propertyName VARCHAR (255) NOT NULL,
+  propertyID INTEGER(11) NOT NULL,
+  p_taskName VARCHAR (255) NOT NULL,
+  p_taskID INTEGER(11) NOT NULL,
+  p_taskStart timestamp NOT NULL,
+  p_taskEnd timestamp NOT NULL,
+  PRIMARY KEY (id)
+);
