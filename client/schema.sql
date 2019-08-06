68cf250729276478cf2bc77601ab2aca200d6bd8
shuh <nonya@biz.com>
2019-08-06 13:28:25 -0400

    created 2 tables in mysql - to be revised

--- client/schema.sql
+++ client/schema.sql
@@ -7,9 +7,14 @@ CREATE TABLE property_list(
   companyName VARCHAR(225) NOT NULL,
   propertyName VARCHAR (255) NOT NULL,
   propertyID INTEGER(11) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE task_list(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
   p_taskName VARCHAR (255) NOT NULL,
   p_taskID INTEGER(11) NOT NULL,
   p_taskStart timestamp NOT NULL,
   p_taskEnd timestamp NOT NULL,
   PRIMARY KEY (id)
-);
+  );
