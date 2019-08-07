aee58e662bed9585c1c4f929930b8de75e842aef
ajkim41 <ajkim41@gmail.com>
2019-08-07 15:35:53 -0400

    Model and Controller

--- routes/api/reservation.js
+++ routes/api/reservation.js
@@ -1,3 +1,17 @@
 const router = require("express").Router();
+const reservationContoller = require("../../controllers/reservationController");
+
+// Matches with "/api/reservation"
+router
+  .route("/")
+  .get(reservationContoller.findAll)
+  .post(reservationContoller.create);
+
+// Matches with "/api/reservation/:id"
+router
+  .route("/:id")
+  .get(reservationContoller.findById)
+  .put(reservationContoller.update)
+  .delete(reservationContoller.remove);
 
 module.exports = router;
