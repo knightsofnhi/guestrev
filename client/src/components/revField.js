12a1cd32b4ec10aa287ced5904d248b6d47d2480
shuh <nonya@biz.com>
2019-08-16 00:42:39 -0400

    merged

--- client/src/components/revField.js
+++ client/src/components/revField.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 
 function RevField(props) {
@@ -27,34 +26,3 @@ function RevField(props) {
 }
 
 export default RevField;
-||||||| merged common ancestors
-=======
-import React, { Component } from "react";
-
-function RevField(props) {
-  return (
-    <tr key={props.id}>
-      <td>
-        <input
-          onChange={props.handleInputChange("company", props.dataIndex)}
-          value={props.company}
-        />
-      </td>
-      <td>
-        <input
-          onChange={props.handleInputChange("property", props.dataIndex)}
-          value={props.property}
-        />
-      </td>
-      <td>
-        <input
-          onChange={props.handleInputChange("task", props.dataIndex)}
-          value={props.task}
-        />
-      </td>
-    </tr>
-  );
-}
-
-export default RevField;
->>>>>>> 8a3507d5afa23bbe13059b98470705009f31151e
