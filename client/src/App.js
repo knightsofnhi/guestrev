400af15c7191c414bc05d6898d5c93153199c3eb
shuh <nonya@biz.com>
2019-08-29 00:02:42 -0400

    logo

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import RevTable from "../src/components/revTable";
-import NavBar from "../src/components/navBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,11 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="..\client\public\assets\img\cendyn.png" className="App-logo" alt="logo" />
           <h2>Welcome to Guestrev</h2>
         </div>
-
-        <NavBar/>
         <RevTable/>
          
 
